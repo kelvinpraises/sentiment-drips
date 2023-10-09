17bcc7bac645b4a758ff853f98b6eeff7193c3d5
kelvinpraises <kelvinpraises@gmail.com>
2023-10-09 23:38:22 +0100

    feat: add pages

---
+++ src/app/ecofunds/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { useEffect, useState } from "react";
+
+import TextHead from "@/components/molecules/TextHead";
+import LargeCard from "@/components/molecules/LargeCard";
+import useBackendAPI, { EcoFund } from "@/hooks/backendAPI";
+
+interface EcoFundWithId extends EcoFund {
+  ecoFundId: number;
+}
+
+interface EcoFundsData {
+  typeIsLink: boolean;
+  href: string;
+  title: string;
+  description: string;
+  buttonText: string;
+  buttonImg: string;
+}
+
+const initData = {
+  title: "Ecosystem EcoFunds",
+  subtitle:
+    "Supporting impactful retroactive project funding for stakeholders and builders",
+  tag: "EcoFund | Grant",
+  cardArray: [] as EcoFundsData[],
+};
+
+const page = () => {
+  const { getEcoFunds } = useBackendAPI();
+  const [data, setData] = useState(initData);
+
+  useEffect(() => {
+    (async () => {
+      const ecoFunds: EcoFundWithId[] = await getEcoFunds("");
+
+      const newEcoFunds = ecoFunds.map((ecoFund) => {
+        return {
+          typeIsLink: true,
+          href: `/grants/ecofunds/${ecoFund.ecoFundId}`,
+          title: ecoFund.title,
+          description: ecoFund.description,
+          buttonText: "Open Fund",
+          buttonImg: "enter.svg",
+        };
+      });
+
+      setData((prev) => {
+        prev.cardArray = newEcoFunds;
+        return prev;
+      });
+    })();
+  }, []);
+
+  return (
+    <div className=" flex-1 bg-white rounded-[10px] p-8 overflow-y-scroll flex flex-col gap-8 shadow-[0px_4px_15px_5px_rgba(226,229,239,0.25)]">
+      <TextHead title={data.title} description={data.subtitle} tag={data.tag} />
+
+      <div className=" grid grid-cols-2 gap-8">
+        {data.cardArray?.map((card, index) => (
+          <LargeCard
+            typeIsLink={card.typeIsLink}
+            href={card.href}
+            key={index}
+            title={card.title}
+            description={card.description}
+            buttonText={card.buttonText}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/showcase-project/page.tsx
@@ -0,0 +1,94 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useReducer } from "react";
+
+import Button from "@/components/atoms/Button";
+import Input from "@/components/atoms/Input";
+import TextHead from "@/components/molecules/TextHead";
+import EmojiPicker from "@/components/organisms/EmojiPicker";
+import useBackendAPI from "@/hooks/backendAPI";
+
+interface NewShowcaseProject {
+  tokensRequested: number;
+  emoji: string;
+  title: string;
+  description: string;
+}
+
+const page = () => {
+  const router = useRouter();
+  const { createProject } = useBackendAPI();
+
+  const [values, updateValues] = useReducer(
+    (
+      current: NewShowcaseProject,
+      update: Partial<NewShowcaseProject>
+    ): NewShowcaseProject => {
+      console.log(update.tokensRequested);
+      return {
+        ...current,
+        ...update,
+        tokensRequested:
+          update.tokensRequested === undefined
+            ? current.tokensRequested
+            : isNaN(current.tokensRequested)
+            ? 0
+            : update.tokensRequested || 0,
+      };
+    },
+    {
+      emoji: "",
+      title: "",
+      tokensRequested: 0,
+      description: "",
+    }
+  );
+
+  console.log(values);
+
+  return (
+    <div className=" flex-1 bg-white rounded-[10px] p-8 overflow-y-scroll flex flex-col gap-8 items-start">
+      <TextHead
+        title="Showcase Project"
+        description="Showcase a project to qualify for an ecosystem funding round."
+      />
+      <div className=" w-[480px] flex flex-col gap-8">
+        <div className=" flex flex-col gap-2 w-full">
+          <p className=" text-sm font-medium">Title</p>
+          <div className=" flex bg-[#DEE6E5] items-center pl-2 rounded-md">
+            <EmojiPicker setSelectedEmoji={updateValues} />
+            <Input
+              input={true}
+              value={values.title}
+              onChange={(e) => updateValues({ title: e.target.value })}
+            />
+          </div>
+        </div>
+        <Input
+          label={"Token Request Amount"}
+          input={true}
+          value={values.tokensRequested.toString()}
+          onChange={(e) =>
+            updateValues({ tokensRequested: parseFloat(e.target.value) })
+          }
+        />
+        <Input
+          label={"Project Description"}
+          input={false}
+          value={values.description}
+          onChange={(e) => updateValues({ description: e.target.value })}
+        />
+      </div>
+      <Button
+        text={"Submit"}
+        handleClick={() =>
+          createProject(values, (d: string) => {
+            router.push("/grants/projects/" + d);
+          })
+        }
+      />
+    </div>
+  );
+};
+
+export default page;
