4fc382ad08fd896e87e378a1aaeded9c74abf2a8
kelvinpraises <kelvinpraises@gmail.com>
2024-01-05 06:49:37 +0100

    feat: make link visible

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ const page = () => {
     <div className=" flex-1 bg-white rounded-[10px] p-8 overflow-y-scroll flex flex-col gap-8 shadow-[0px_4px_15px_5px_rgba(226,229,239,0.25)]">
       <TextHead title={data.title} description={data.subtitle} tag={data.tag} />
 
-      <Link href={data.linkHref || ""} className=" font-semibold">
+      <Link href={data.linkHref || ""} className="w-fit font-bold underline underline-offset-2">
         {data.link}
       </Link>
 
