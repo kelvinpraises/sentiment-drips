d91c2a8852235f719dc0a3f9c31ca82df943abfa
kelvinpraises <kelvinpraises@gmail.com>
2024-01-08 20:47:38 +0100

    chore: refactor description

--- library/components/organisms/VoteSection.tsx
+++ library/components/organisms/VoteSection.tsx
@@ -1,18 +1,19 @@
 import Button from "../atoms/Button";
 
-const VoteSection = ({ ecoFundId }: { ecoFundId: any }) => (
-  <div className="flex flex-col gap-4">
-    <div className=" flex justify-between">
-      <p className=" text-sm">
-        Vote on whether the proposal fits into the ecosystem
-      </p>
-      <Button text={"Vote on Proposal"} handleClick={undefined} />
+const VoteSection = ({ ecoFundId }: { ecoFundId: any }) => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className=" flex justify-between">
+        <p className=" text-sm">
+          Vote on if the collective averaged sentiment is fit for the ecosystem
+        </p>
+        <Button text={"Vote on Proposal"} handleClick={undefined} />
+      </div>
+      <div className="flex flex-col gap-8">
+        <div className="w-[520px]">{}</div>
+      </div>
     </div>
-    <div className="flex flex-col gap-8">
-      <p className="font-bold text-xl">Vote Info</p>
-      <div className="w-[520px]">{}</div>
-    </div>
-  </div>
-);
+  );
+};
 
 export default VoteSection;
