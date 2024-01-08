7e3e82717e660eea053123bbbbf27e1bdd9fe8db
kelvinpraises <kelvinpraises@gmail.com>
2024-01-08 22:30:41 +0100

    feat: add disabled state

--- library/components/atoms/Input.tsx
+++ library/components/atoms/Input.tsx
@@ -1,8 +1,10 @@
+import { cn } from "@/library/utils";
 import { ChangeEvent } from "react";
 
 interface inputProps {
   type?: string;
   label?: string;
+  disabled?: boolean;
   input: boolean;
   value: string;
   onChange: (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
@@ -10,20 +12,30 @@ interface inputProps {
 
 const Input = (prop: inputProps) => {
   return (
-    <div className=" flex flex-col gap-2 w-full">
+    <div className="flex flex-col gap-2 w-full">
       {prop.label && <p className=" text-sm font-medium">{prop.label}</p>}
       {prop.input ? (
         <input
           type={prop.type ? prop.type : "text"}
-          className=" bg-[#DEE6E5] p-4 rounded-md outline-none w-full"
+          className={cn(
+            "bg-[#DEE6E5] p-4 rounded-md outline-none w-full",
+            prop.disabled &&
+              "bg-white shadow-[0_0_0_2px] shadow-[#DEE6E5] outline-none"
+          )}
           value={prop.value}
           onChange={prop.onChange}
+          disabled={prop.disabled}
         />
       ) : (
         <textarea
-          className=" bg-[#DEE6E5] p-4 rounded-md h-[113px] outline-none"
+          className={cn(
+            "bg-[#DEE6E5] p-4 rounded-md h-[113px] outline-none",
+            prop.disabled &&
+              "bg-white shadow-[0_0_0_2px] shadow-[#DEE6E5] outline-none"
+          )}
           value={prop.value}
           onChange={prop.onChange}
+          disabled={prop.disabled}
         />
       )}
     </div>
