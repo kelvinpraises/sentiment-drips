8bcc242f6dbfb251d174775b547f77da5c9b5872
kelvinpraises <kelvinpraises@gmail.com>
2024-01-15 22:30:30 +0100

    chore: update class names

--- library/components/atoms/Button.tsx
+++ library/components/atoms/Button.tsx
@@ -13,7 +13,7 @@ const Button = (button: buttonProps) => {
     <>
       {button.link ? (
         <Link
-          className=" py-4 px-8 rounded-[5px] text-sm font-semibold bg-[#313B3D] text-white flex items-center"
+          className=" py-4 px-8 rounded-[5px] text-sm font-semibold bg-[#313B3D] text-white flex items-center justify-center"
           href={button.href || ""}
         >
           {button.text}
@@ -24,7 +24,7 @@ const Button = (button: buttonProps) => {
       ) : (
         <button
           onClick={button.handleClick}
-          className=" py-4 px-8 rounded-[5px] text-sm font-semibold bg-[#313B3D] text-white flex items-center"
+          className=" py-4 px-8 rounded-[5px] text-sm font-semibold bg-[#313B3D] text-white flex items-center justify-center"
         >
           {button.text}
           {button.buttonImg && (
